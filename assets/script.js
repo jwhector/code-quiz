f9aba6e19e16838e2e1b22a7f8a79e8bea15d6b0
jwhector <jared.hector@gmail.com>
2021-09-27 17:55:34 -0700

    Add styling and button functionality

--- assets/script.js
+++ assets/script.js
@@ -5,16 +5,25 @@ var button3 = document.querySelector('#button-3');
 var button4 = document.querySelector('#button-4');
 var question = document.querySelector('#question');
 var image = document.querySelector('#question-image');
+var startButton = document.querySelector('#start-button');
+var startBox = document.querySelector('.start');
+var questionBox = document.querySelector('.question-box');
+var imageBox = document.querySelector('.image-box');
+var confirmation = document.querySelector('#confirmation');
+var shuffledQuestions = [];
+var curIdx = 0;
+var remainingTime = 90;
+var curQuestion, timerInterval;
 
 var questions = [
     {
-        question: "What is the title of the album shown?",
+        question: "What is the title of the album from the art shown?",
         A: "Glass Animals",
-        B: "ZABA",
-        C: "Dreamland",
+        B: "Dreamland",
+        C: "ZABA",
         D: "How to Be a Human Being",
         answer: "B",
-        image: "zaba.png"
+        image: "dreamland.jpg"
     },
     {
         question: "For which popular UK music festival (shown) is Glass Animals known for?",
@@ -45,3 +54,67 @@ var questions = [
     },
 ];
 
+function initialize() {
+    shuffledQuestions = questions.sort(() => 0.5 - Math.random());
+    
+    timerInterval = setInterval(function () {
+        remainingTime--;
+        if (remainingTime <= 0) {
+            timer.textContent = 'You lose!';
+            clearInterval(timerInterval);
+        } else {
+            timer.textContent = `Time: ${remainingTime}`;
+        }
+    }, 1000);
+}
+
+function start(event) {
+    startBox.classList.add('hidden');
+    questionBox.classList.remove('hidden');
+    imageBox.classList.remove('hidden');
+
+    displayQuestion();
+}
+
+function displayQuestion() {
+    if (remainingTime <= 0) {
+        return;
+    }
+
+    curQuestion = shuffledQuestions[curIdx];
+    curIdx++;
+
+    console.log(shuffledQuestions);
+
+    question.textContent = curQuestion.question;
+    button1.textContent = curQuestion.A;
+    button2.textContent = curQuestion.B;
+    button3.textContent = curQuestion.C;
+    button4.textContent = curQuestion.D;
+    image.src = `./assets/images/${curQuestion.image}`;
+}
+
+function checkAnswer(event) {
+    console.log(event);
+    console.log(event.target.dataset.label);
+
+    if (event.target.dataset.label === curQuestion.answer) {
+        confirmation.textContent = "Correct!";
+        displayQuestion();
+    } else {
+        confirmation.textContent = "Incorrect."
+        setTimeout(function () {
+            confirmation.textContent = '';
+        }, 2000);
+        remainingTime -= 10;
+        // timer.textContent = `Time: ${remainingTime}`;
+    }
+}
+
+initialize();
+
+startButton.addEventListener('click', start);
+button1.addEventListener('click', checkAnswer);
+button2.addEventListener('click', checkAnswer);
+button3.addEventListener('click', checkAnswer);
+button4.addEventListener('click', checkAnswer);
